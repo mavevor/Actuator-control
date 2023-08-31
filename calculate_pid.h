a9c4a58a2ead7698866eb2275d5df03a31c4e956
mavevor <98754133+mavevor@users.noreply.github.com>
2023-08-31 20:43:18 +0100

    Update calculate_pid.h
    fixed force calculation

--- calculate_pid.h
+++ calculate_pid.h
@@ -4,8 +4,8 @@ uint8_t PID_ON_STATE[3] = {0};
 int pid_state_index = -1;
 int pid_key_index = -1;
 
-int force_threshold[3] = {600, 600, 600};
-float max_dist[3] = {0};
+int force_threshold[3] = {920, 600, 600};
+float max_dist[3] = {255,255,255};
 float min_dist[3] = {0};
 const int force_buffer_len = 10;
 float force_buffer_0[force_buffer_len] = {0};
@@ -19,14 +19,15 @@ float i_error[3] = {0};
 float d_error[3] = {0};
 float p_error_prev[3] = {0};
 
-float K_p[3] = {0};
-float K_i[3] = {0};
-float K_d[3] = {0};
+float K_p[3] = {0.001,0,0};
+float K_i[3] = {0.0001,0,0};
+float K_d[3] = {1,0,0};
 
-int dist[3] = {0};
+uint8_t dist[3] = {0};
+float corrected_dist[3] = {0};
 
 int ACTUATOR[3] = {8,9,10};
-int force_sensor[3] = {11,12,13};
+int force_sensor[3] = {26,27,28};
 
 double loop_timer = 0;
 double cycle_time = 4000;
@@ -34,10 +35,10 @@ int force_counter = 0;
 
 uint8_t force_data_out = 0;
 
+double force_timer = 0;
+
 void measure_force(void){ 
-  av_force[0] = 0;
-  av_force[1] = 0;
-  av_force[2] = 0;
+  
   force_counter = 0;
   force_data_out = 32;                      // shifted left twice to give 0b10000000
 
@@ -46,9 +47,14 @@ void measure_force(void){
       force_buffer_0[force_counter%force_buffer_len] = analogRead(force_sensor[0]);
       force_buffer_1[force_counter%force_buffer_len] = analogRead(force_sensor[1]);
       force_buffer_2[force_counter%force_buffer_len] = analogRead(force_sensor[2]);
-      av_force[0] += force_buffer_0[force_counter%force_buffer_len]/(force_counter+1);
-      av_force[1] += force_buffer_1[force_counter%force_buffer_len]/(force_counter+1);
-      av_force[2] += force_buffer_2[force_counter%force_buffer_len]/(force_counter+1);
+      av_force[0] = 0;
+      av_force[1] = 0;
+      av_force[2] = 0;
+      for (int i = 0; i <= force_counter; i++){
+      av_force[0] += force_buffer_0[i]/(force_counter+1);
+      av_force[1] += force_buffer_1[i]/(force_counter+1);
+      av_force[2] += force_buffer_2[i]/(force_counter+1);
+      }
       force_counter ++;
     }
     if (force_counter >= force_buffer_len){
@@ -61,13 +67,15 @@ void measure_force(void){
       av_force[0] += force_buffer_0[force_counter%force_buffer_len]/force_buffer_len;
       av_force[1] += force_buffer_1[force_counter%force_buffer_len]/force_buffer_len;
       av_force[2] += force_buffer_2[force_counter%force_buffer_len]/force_buffer_len;
+      force_counter ++;
     }
   }
   for(int i = 0; i < 3; i ++){
-    force_data_out += (av_force[i] > force_threshold[i]);
+    force_data_out += ((av_force[i] > 0.96*force_threshold[i])); //||(corrected_dist[i] == max_dist[i]));
     if(i < 2){force_data_out = force_data_out << 1;} 
   }
   loop_timer = micros();
+
 }
 
 void calculate_pid(void){                                                                   // cycle time is constant.
@@ -79,14 +87,16 @@ void calculate_pid(void){                                                       
       d_error[i] = p_error[i] - p_error_prev[i];
       p_error_prev[i] = p_error[i];
 
-      if (K_i[i] * i_error[i] > max_dist[i] && K_i != 0){i_error[i] = max_dist[i] / K_i[i];}      
-      if (K_i[i] * i_error[i] < min_dist[i] && K_i != 0){i_error[i] = min_dist[i] / K_i[i];}
+      if ((K_i[i] * i_error[i] > max_dist[i]) && K_i[i] != 0){i_error[i] = max_dist[i] / K_i[i];}      
+      if ((K_i[i] * i_error[i] < min_dist[i]) && K_i[i] != 0){i_error[i] = min_dist[i] / K_i[i];}
 
-      dist[i] = K_p[i] * p_error[i] + K_i[i] * i_error[i] + K_d[i] * d_error[i];
-
-      if (dist[i] > max_dist[i]){dist[i] = max_dist[i];}
-      if (dist[i] < max_dist[i]){dist[i] = max_dist[i];}
+      corrected_dist[i] = K_p[i] * p_error[i] + K_i[i] * i_error[i] + K_d[i] * d_error[i];
+      if (corrected_dist[i] > max_dist[i]){corrected_dist[i] = max_dist[i];}
+      if (corrected_dist[i] < min_dist[i]){corrected_dist[i] = min_dist[i];}
     }
-    analogWrite(ACTUATOR[i], dist[i]);
+    else{
+      corrected_dist[i] = dist[i];
+    }
+    analogWrite(ACTUATOR[i], corrected_dist[i]);
   }
 }
