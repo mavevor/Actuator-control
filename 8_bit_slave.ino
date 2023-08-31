fa9a61583b380a485553b27b66d74a5bc0084b3c
mavevor <98754133+mavevor@users.noreply.github.com>
2023-08-31 20:41:28 +0100

    Update 8_bit_slave.ino
    added timer for robustness

--- 8_bit_slave.ino
+++ 8_bit_slave.ino
@@ -12,8 +12,7 @@ int state_index = -1;
 uint8_t in_byte = 0;
 uint8_t out_byte= 0;
 
-
-
+double timer_1 = 0;
 
 int index(uint8_t arr[], uint8_t c, int len = 3){
   for (int i = 0; i < len; ++i)
@@ -33,24 +32,42 @@ void setup() {
   for (int i = 0; i < 3; i++){
   analogWrite(ACTUATOR[i],dist[i]);    
   }
-  //write_data(out_byte);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  /*while (input_buffer_counter == 0){
-    //sensor_val = analogRead(A0);
-    //Serial.print('\n');
-    //Serial.print("Sensor value: ");
-    //Serial.println(sensor_val); 
-  }*/
+  int temp_counter = input_buffer_counter;
+  timer_1 = micros();
+  while(ext_error_val != 0){
+    if ((micros() - timer_1) < 1000){
+      write_data(RESET_KEY); 
+      for (int i = 0; i < 3; i++){
+        dist[i] = 0;
+        ON_STATE[i] = 0;
+        PID_STATE[i] = 0;
+        PID_ON_STATE[i] = 0;
+      }
+    }
+    else{
+      delayMicroseconds(1000);  
+      timer_1 = micros();    
+    }
+  }
+  
+  measure_force();
+  calculate_pid();
+  if (temp_counter == 0 && (micros() - force_timer > 100000)){
+    write_data(force_data_out);
+    force_timer = micros();
+  }
+  
   double t_1 = micros();
   double t_2 = 0;
-  if (input_buffer_counter > 1){
+  if (temp_counter > 1){
     in_byte = read_data();   
   }
 
-  if (input_buffer_counter == 1){
+  if (temp_counter == 1){
     
     in_byte = read_data();
     key_index = index(KEY_LIST,in_byte);
@@ -65,16 +82,12 @@ void loop() {
         PID_STATE[i] = 0;
         PID_ON_STATE[i] = 0;
       }
-      t_2 = micros() - t_1;
-      Serial.println(t_2);
     }
     
-    if ((key_index +1) && !(state_index + 1)){                    // key_index + 1 non zero if in_byte in KEY_LIST
+    else if ((key_index +1) && !(state_index + 1)){                    // key_index + 1 non zero if in_byte in KEY_LIST
       out_byte = KEY_LIST[key_index] - 1;                         // state_index + 1 zero if all states are 0
       write_data((byte) out_byte);
       ON_STATE[key_index] = 1;
-      t_2 = micros() - t_1;
-      Serial.println(t_2);
     }
 
 
@@ -85,8 +98,6 @@ void loop() {
       
       ON_STATE[state_index] = 0;
       PID_STATE[state_index] = 1;
-      t_2 = micros() - t_1;
-      Serial.println(t_2);
     }
     else if((pid_key_index + 1) && (pid_state_index + 1)){
       out_byte = PID_KEY_LIST[pid_key_index];
@@ -96,22 +107,15 @@ void loop() {
 
     }
 
-    //Serial.print("  ");
-    //Serial.println(out_byte);
     if(pid_state_index + 1){
-      t_2 = micros() - t_1;
-      Serial.println(t_2);
       analogWrite(ACTUATOR[pid_state_index],dist[pid_state_index]);
       max_dist[pid_state_index] = 1.1 * dist[pid_state_index];          //pid controller acts over distances +- 10% of set value
+      if (max_dist[pid_state_index] > 255){max_dist[pid_state_index] = 255;}
       min_dist[pid_state_index] = 0.9 * dist[pid_state_index];
       delayMicroseconds(1000000);                                       //wait for actuator to get to position
       loop_timer = micros();
     }
   }
-  measure_force();
-  calculate_pid();
-  if (input_buffer_counter == 0){
-    write_data(force_data_out);
-  }
+
 
 }
